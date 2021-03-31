be91c636350ec4084faf9d31103cf1d8ed58f6ca
saabismi <vilho.syvahuoko@gmail.com>
2021-03-31 15:42:55 +0300

    add deadline field as date, start setting up json as storage

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-const form = document.getElementById("newObjective");
+// variables for the dom elements
 const inputField = document.getElementById("inputField"); // text area where you type your objective
 const saveTodo = document.getElementById("saveTodo"); // html button element which saves the text in the input field
+const setDate = document.getElementById("inputDate"); // the date the user set as the deadline
 const objListElem = document.getElementById("objectives"); // html ul element in which the saved objectives are shown in
+
+//const xhttp = new XMLHttpRequest();
+
+const todoStorage  = window.localStorage;
 
 let objList = []; // initialise array object for the objectives
 
@@ -12,6 +17,20 @@ function showObjConsole() {
     }
 }
 
+/*
+xhttp.onreadystatechange = function () {
+    if(this.readyState == 4 && this.status == 200) {
+        let todos = JSON.parse(this.responseText);
+
+        for(let i = 0; i < todos.length; i++) {
+
+        }
+    }
+}
+*/
+
+fetch("http://localhost:3000/todos").then(response => response.json()).then(data => console.log(data));
+
 saveTodo.addEventListener("click", function() {
     if(inputField.value == "") {
         alert("You can't save an empty objective!");
@@ -21,7 +40,9 @@ saveTodo.addEventListener("click", function() {
         newLi.innerHTML = inputField.value; //create a li element with the objective innit bruv
         objListElem.appendChild(newLi); // append the newly created li element to the unordered list where the objectives are shown 
         inputField.value = ""; // clear the text field
+        console.log("date: " + setDate.value);
         showObjConsole();
+        //todoStorage.setItem("todos", JSON.stringify(objList));
     }
 });
 
@@ -36,6 +57,7 @@ inputField.addEventListener("keypress", function (event) {
             newLi.innerHTML = inputField.value; //create a li element with the objective innit bruv
             objListElem.appendChild(newLi); // append the newly created li element to the unordered list where the objectives are shown 
             inputField.value = ""; // clear the text field
+            console.log("date: " + setDate.value);
             showObjConsole();
         }
     }
